687fb7ef63debb734c94be97e171bb80cd1bbde6
Leo <leozinho17silvacosta@gmail.com>
2025-09-24 20:47:49 -0700

    Complete exercise 8

--- ex08.js
+++ ex08.js
@@ -1,3 +1,6 @@
 // Join Exercise
 // Instructions: Create an array of at least five different words as strings. Write code to join all the words into a single string, with each word separated by a space.
 
+const diffWords = ["ball", "lemon", "mouse", "chair", "headphone"];
+
+console.log(diffWords.join(" "));
